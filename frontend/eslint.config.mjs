346436982b3e7b984d245bfa9c47377aa8a6db23
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-07-09 13:34:23 +0200

    lint locales

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,28 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import vueI18n from '@intlify/eslint-plugin-vue-i18n'
 
 export default withNuxt(
   // Your custom configs here
+  ...vueI18n.configs.recommended,
+  {
+    rules: {
+      // Optional.
+      '@intlify/vue-i18n/no-dynamic-keys': 'error',
+      '@intlify/vue-i18n/no-unused-keys': [
+        'error',
+        {
+          extensions: ['.ts', '.vue']
+        }
+      ]
+    },
+    settings: {
+      'vue-i18n': {
+        localeDir: './locales/*.{json}', // extension is glob formatting!
+        // Specify the version of `vue-i18n` you are using.
+        // If not specified, the message will be parsed twice.
+        messageSyntaxVersion: '^11.0.0'
+      }
+    }
+  }
 )
