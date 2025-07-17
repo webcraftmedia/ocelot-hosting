2026043ae737d67b50286a99d55082a0825ce799
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-07-17 18:14:02 +0200

    fix vue warning in tests

--- frontend/src/components/Section.spec.ts
+++ frontend/src/components/Section.spec.ts
@@ -5,7 +5,7 @@ import { describe, expect, it } from 'vitest'
 
 describe('Section', () => {
   it('renders', async () => {
-    const html = await (await renderSuspended(Component)).html()
+    const html = await (await renderSuspended(Component,{props: {id: 'test-section'}})).html()
     expect(html).toMatchSnapshot()
   })
 })
