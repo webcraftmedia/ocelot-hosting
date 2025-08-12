502c2ec497b850a47c6103f4c09e1c3c19281720
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-08-12 23:43:13 +0200

    basic idea scetch for testimonials

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -48,11 +48,134 @@
             </div>
         </Section>
 
-        <Section id="community" class="bg-blue-300">
+        <Section id="community">
             <h2>{{ $t('pages.index.community.headline') }}</h2>
             <p>{{ $t('pages.index.community.subtitle') }}</p>
-            <!-- eslint-disable-line @intlify/vue-i18n/no-raw-text -->
-            testimonials
+        
+            
+
+            <div id="animation-carousel" class="relative w-full" data-carousel="static">
+                <!-- Carousel wrapper -->
+                <div class="relative h-120 overflow-hidden rounded-lg">
+                    <!-- Item 1 -->
+                    <div class="duration-200 ease-linear" data-carousel-item>
+                        <div class="max-w-screen-xl mx-auto text-center h-100">
+                            <figure class="max-w-screen-md mx-auto">
+                                <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600 mt-10" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+                                </svg> 
+                                <blockquote>
+                                    <p class="text-2xl font-medium text-gray-900 dark:text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                                </blockquote>
+                                <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                                    <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture">
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <div class="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
+                                        <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <!-- Item 2 -->
+                    <div class="hidden duration-200 ease-linear" data-carousel-item>
+                        <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+                            <figure class="max-w-screen-md mx-auto">
+                                <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+                                </svg> 
+                                <blockquote>
+                                    <p class="text-2xl font-medium text-gray-900 dark:text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                                </blockquote>
+                                <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                                    <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture">
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <div class="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
+                                        <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <!-- Item 3 -->
+                    <div class="hidden duration-200 ease-linear" data-carousel-item="active">
+                        <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+                            <figure class="max-w-screen-md mx-auto">
+                                <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+                                </svg> 
+                                <blockquote>
+                                    <p class="text-2xl font-medium text-gray-900 dark:text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                                </blockquote>
+                                <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                                    <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture">
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <div class="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
+                                        <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <!-- Item 4 -->
+                    <div class="hidden duration-200 ease-linear" data-carousel-item>
+                        <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+                            <figure class="max-w-screen-md mx-auto">
+                                <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+                                </svg> 
+                                <blockquote>
+                                    <p class="text-2xl font-medium text-gray-900 dark:text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                                </blockquote>
+                                <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                                    <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture">
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <div class="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
+                                        <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <!-- Item 5 -->
+                    <div class="hidden duration-200 ease-linear" data-carousel-item>
+                        <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+                            <figure class="max-w-screen-md mx-auto">
+                                <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+                                </svg> 
+                                <blockquote>
+                                    <p class="text-2xl font-medium text-gray-900 dark:text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                                </blockquote>
+                                <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                                    <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture">
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <div class="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
+                                        <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <!-- Slider controls -->
+                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
         </Section>
 
         <Section id="try" class="bg-yellow-300">
@@ -99,21 +222,16 @@ import ImgFeatureFollowers from '~/../assets/followers.png'
 #features {
     @apply bg-green-300;
     @apply p-15;
-
-    p {
-        @apply text-4xl;
-        @apply pb-8;
-    }
 }
 
 #more {
     @apply bg-pink-300;
     @apply p-15;
+}
 
-    p {
-        @apply text-4xl;
-        @apply pb-8;
-    }
+#community {
+    @apply bg-blue-300;
+    @apply p-15;
 }
 
 h1 {
@@ -124,6 +242,11 @@ h1 {
 h2 {
     @apply text-6xl;
     @apply pb-4;
+}
+
+p {
+    @apply text-4xl;
+    @apply pb-8;
 }
 
 </style>
