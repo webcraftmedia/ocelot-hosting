c481e3f3dfd53043837c4a4b121892fb5b029c27
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-09-04 23:10:49 +0100

    fix(frontend): fix i18n (#22)
    * fix i18n
    
    * fix language switch
    
    * more fixes
    
    * remove unused tests
    
    * back to 100% coverage
    
    * cleanup

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,6 @@
-const DOMAIN_EN = process.env.DOMAIN_EN ?? 'https://localhost:3000'
-const DOMAIN_DE = process.env.DOMAIN_DE ?? 'https://localhost:3000'
+const DOMAIN_EN = process.env.DOMAIN_EN ?? 'http://localhost:3000'
+const DOMAIN_DE = process.env.DOMAIN_DE ?? 'http://localhost:3000'
 
-const languageDomains = [
-  DOMAIN_EN,
-  DOMAIN_DE,
-]
 
 
 export default defineNuxtConfig({
@@ -19,18 +15,16 @@ export default defineNuxtConfig({
   i18n: {
     restructureDir: './',
     defaultLocale: 'en',
-    // baseUrl: DOMAIN_DE,
-    // differentDomains: process.env.NODE_ENV === 'production',
-    multiDomainLocales: process.env.NODE_ENV === 'production',
+    differentDomains: process.env.NODE_ENV === 'production',
     locales: [
-      { code: 'en', name: 'English', file: 'en.json', /* domain: DOMAIN_EN, */ domains: languageDomains, defaultForDomains: [DOMAIN_EN] /*, domainDefault: true */ },
-      { code: 'de', name: 'Deutsch', file: 'de.json', /* domain: DOMAIN_DE, */ domains: languageDomains, defaultForDomains: [DOMAIN_DE] /*,  domainDefault: true */ }
+      { code: 'en', name: 'English', file: 'en.json', domain: DOMAIN_EN },
+      { code: 'de', name: 'Deutsch', file: 'de.json', domain: DOMAIN_DE }
     ],
     // detectBrowserLanguage: false,
     detectBrowserLanguage: {
       // This doesn't make a difference
       useCookie: false,
-      alwaysRedirect: true
+      alwaysRedirect: true,
     },
     strategy: 'no_prefix',
     bundle: {
--- frontend/src/components/Header.spec.ts
+++ frontend/src/components/Header.spec.ts
@@ -1,9 +1,13 @@
 
 import { renderSuspended } from '@nuxt/test-utils/runtime'
 import Component from './Header.vue'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi, beforeEach } from 'vitest'
 
 describe('Header', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
   it('renders', async () => {
     const html = await (await renderSuspended(Component)).html()
     expect(html).toMatchSnapshot()
