3ef70471ced1e3ecce989e432093634bb64fa47d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-02 09:59:58 +0100

    mobile optimization

--- frontend/src/pages/try.vue
+++ frontend/src/pages/try.vue
@@ -50,12 +50,16 @@
 #try {
   @apply bg-slate-50;
   @apply text-center;
-  @apply pt-24;
-  @apply pb-16;
-  @apply px-8;
+  @apply pt-20;
+  @apply pb-12;
+  @apply px-6;
+  @apply md:pt-24;
+  @apply md:pb-16;
+  @apply md:px-8;
 
   h1 {
-    @apply text-5xl;
+    @apply text-3xl;
+    @apply md:text-5xl;
     @apply font-bold;
     @apply pb-4;
   }
@@ -123,10 +127,12 @@
   @apply text-white;
   @apply text-center;
   @apply py-20;
-  @apply px-8;
+  @apply px-6;
+  @apply md:px-8;
 
   h2 {
-    @apply text-5xl;
+    @apply text-3xl;
+    @apply md:text-5xl;
     @apply font-bold;
     @apply text-white;
     @apply pb-4;
