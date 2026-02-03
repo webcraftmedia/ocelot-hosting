cc945f1859f321b52a434764da95bd4af9eeed42
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-03 07:08:09 +0100

    refactor foorter

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,22 +1,17 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-        >{{ $t('components.Footer.copyright') }}
-        <a href="https://www.webcraft-media.de/" class="hover:underline">{{
-          $t('components.Footer.copyright-holder')
-        }}</a
-        >{{ $t('components.Footer.copyright-text') }}
-      </span>
+  <footer class="bg-white border-t border-gray-100 dark:bg-gray-800 dark:border-gray-700">
+    <div
+      class="w-full mx-auto max-w-screen-xl px-6 py-6 flex flex-col items-center gap-4 md:flex-row md:justify-between md:py-4"
+    >
       <ul
-        class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+        class="flex flex-wrap justify-center gap-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:order-2"
       >
         <li>
           <NuxtLink
             :to="{ path: 'https://www.webcraft-media.de/#!impressum' }"
             external
-            class="hover:underline me-4 md:me-6"
+            class="hover:underline"
           >
             {{ $t('components.Footer.imprint') }}
           </NuxtLink>
@@ -25,20 +20,25 @@
           <NuxtLink
             :to="{ path: 'https://www.webcraft-media.de/#!datenschutz' }"
             external
-            class="hover:underline me-4 md:me-6"
+            class="hover:underline"
           >
             {{ $t('components.Footer.privacy-policy') }}
           </NuxtLink>
         </li>
         <li>
-          <a href="#" class="hover:underline me-4 md:me-6">{{
-            $t('components.Footer.licensing')
-          }}</a>
-        </li>
-        <li>
-          <a href="#" class="hover:underline">{{ $t('components.Footer.contact') }}</a>
+          <NuxtLink :to="{ path: '/try', hash: '#demo' }" class="hover:underline">
+            {{ $t('components.Footer.contact') }}
+          </NuxtLink>
         </li>
       </ul>
+      <span
+        class="text-sm text-gray-500 text-center dark:text-gray-400 md:order-1"
+        >{{ $t('components.Footer.copyright') }}
+        <a href="https://www.webcraft-media.de/" class="hover:underline">{{
+          $t('components.Footer.copyright-holder')
+        }}</a
+        >{{ $t('components.Footer.copyright-text') }}
+      </span>
     </div>
   </footer>
 </template>
