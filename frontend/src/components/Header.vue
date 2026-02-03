718c0705282309095f5ca74fbdd9e35ad0d72809
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-03 06:08:28 +0100

    mobile menu ctas

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -34,13 +34,13 @@
       </button>
       <div id="navbar-default" :class="{ hidden: !isMenuOpen }" class="w-full md:block md:w-auto">
         <ul
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 justify-center items-center"
+          class="font-medium flex flex-col p-4 md:p-0 mt-2 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:bg-white dark:bg-gray-900 justify-center items-center"
         >
           <li>
             <NuxtLink
               :to="{ path: '/', hash: '#features' }"
-              class="block py-2 px-3 text-white bg-teal-700 rounded-sm md:bg-transparent md:text-teal-700 md:p-0 dark:text-white md:dark:text-teal-400"
-              aria-current="page"
+              class="block py-2 px-3 text-gray-900 md:text-teal-700 md:p-0 dark:text-white md:dark:text-teal-400"
+              @click="isMenuOpen = false"
             >
               {{ $t('components.Header.features') }}
             </NuxtLink>
@@ -48,7 +48,8 @@
           <li>
             <NuxtLink
               :to="{ path: '/', hash: '#community' }"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              class="block py-2 px-3 text-gray-900 md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-400"
+              @click="isMenuOpen = false"
             >
               {{ $t('components.Header.community') }}
             </NuxtLink>
@@ -56,12 +57,31 @@
           <li>
             <NuxtLink
               :to="{ path: '/pricing' }"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              class="block py-2 px-3 text-gray-900 md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-400"
+              @click="isMenuOpen = false"
             >
               {{ $t('components.Header.pricing') }}
             </NuxtLink>
           </li>
-          <li>
+          <li class="md:hidden">
+            <NuxtLink
+              :to="{ path: '/try', hash: '#try' }"
+              class="block py-2 px-3 text-teal-700 font-semibold dark:text-teal-400"
+              @click="isMenuOpen = false"
+            >
+              {{ $t('cta.try') }}
+            </NuxtLink>
+          </li>
+          <li class="md:hidden">
+            <NuxtLink
+              :to="{ path: '/try', hash: '#demo' }"
+              class="block py-2 px-3 text-teal-700 font-semibold dark:text-teal-400"
+              @click="isMenuOpen = false"
+            >
+              {{ $t('cta.book') }}
+            </NuxtLink>
+          </li>
+          <li class="hidden md:flex gap-2">
             <NuxtLink :to="{ path: '/try', hash: '#try' }">
               <Button type="primary" :text="$t('cta.try')" />
             </NuxtLink>
@@ -69,11 +89,12 @@
               <Button type="secondary" :text="$t('cta.book')" />
             </NuxtLink>
           </li>
-          <li>
+          <li class="py-2 md:py-0">
             <a
               v-for="av in availableLocales"
               :key="av.code"
-              @click.prevent="handleLanguageSwitch(av.code)"
+              class="cursor-pointer"
+              @click.prevent="handleLanguageSwitch(av.code); isMenuOpen = false"
             >
               <div class="inline-flex items-center">
                 <svg
